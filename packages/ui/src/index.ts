16170bf2370c90372b5c7229dd4be3ce3456d9e4
Jesse Pollak <jpollak@coinbase.com>
2026-09-11 23:22:12 -0700

    feat(ui): add shared press feedback
    Co-authored-by: Toshi <toshi-noreply@coinbase.com>

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,3 +1,4 @@
 export { Text, Heading, type TextProps, type HeadingProps, type TextStyle } from "./text";
 export { Button, type ButtonProps } from "./button";
 export { IconButton, type IconButtonProps } from "./icon-button";
+export { haptic, type HapticFeedback } from "./haptic";
